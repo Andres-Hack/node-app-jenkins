cff7cfcc0569ead9b4894d3c338c0108c1b48284
Andres-Hack <andresmamanielt@gmail.com>
2018-10-13 17:02:15 -0400

    subido con con exito para la prueba mail

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var app = require('../index.js');
 
 describe('GET /', function() {
     it('respond with hello world', function(done) {
-    request().get('/').expect('hello world', done);
+    request(app).get('/').expect('hello world', done);
  });
 });
 
