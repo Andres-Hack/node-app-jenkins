40fcbbe9d87b1b024cc5195d7aaacfa22b4dcf83
Andres-Hack <andresmamanielt@gmail.com>
2018-10-13 17:00:19 -0400

    subido con error para la prueba mail

--- test/test.js
+++ test/test.js
@@ -3,6 +3,9 @@ var app = require('../index.js');
 
 describe('GET /', function() {
     it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world', done);
+    request().get('/').expect('hello world', done);
  });
 });
+
+// npm install
+// ./script/test
